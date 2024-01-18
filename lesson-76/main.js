78aa79aa904983e1473e8a5103cfe2051d127cb9
Hrabroid <153987798+Hrabroid@users.noreply.github.com>
2024-01-18 18:21:11 +0500

    add lesson-76

---
+++ lesson-76/main.js
@@ -0,0 +1,19 @@
+const firstName = 'Алекс';
+
+lastName = 'Храбрых';
+
+let $ = 15;
+
+let _ = 21;
+
+console.log(firstName, lastName, $ + _, 'лет отроду');
+
+let message = 'Всем привет!';
+
+alert(message);
+
+
+let myInt = 9;
+let myFloat = 8;
+
+console.log(myInt * myFloat, 'Регион Тюмень');
