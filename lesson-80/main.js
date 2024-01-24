848ae618b9154f17bfa1e009ae1bac419353d72b
Hrabroid <153987798+Hrabroid@users.noreply.github.com>
2024-01-24 16:21:54 +0500

    add lesson-80

---
+++ lesson-80/main.js
@@ -0,0 +1,26 @@
+let firstTask = 'First task'
+console.log(firstTask)
+
+
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log(i)
+}
+
+let secondTask = 'Second task'
+console.log(secondTask)
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
+}
+
+let i = 2
+while(i <= 12) {
+    alert(i)
+    i = i + 2
+}
+
+let j = 10
+while(j >= 1) {
+    alert(j)
+    j--
+}
